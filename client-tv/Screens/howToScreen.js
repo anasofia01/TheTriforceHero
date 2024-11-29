2a21a96b061d622a497bd617270547c5f457536a
Azuki19 <analucia27259@gmail.com>
2024-11-29 05:28:53 -0500

    css2

--- client-tv/Screens/howToScreen.js
+++ client-tv/Screens/howToScreen.js
@@ -4,15 +4,15 @@ export default function renderScreen5() {
 	const app = document.getElementById('app');
 	app.innerHTML = `
   <section id='howToSection' >
-  <section>
-    <h1>How to Play?</h1>
-    <p>You are the chosen hero, tasked with protecting the Triforce. Defend Hyrule by fighting off the monsters send by Ganon that threaten the peace!</p>
-		<img id='battleLinkPig' src='https://pbs.twimg.com/media/GaH35BiWgAAMhNi?format=jpg&name=4096x4096'/>
-  </section>
+    <section id='howToSection1'>
+      <h1 id='howtoplayh1'>How to Play?</h1>
+      <p id='howtoplayp'>You are the chosen hero, tasked with protecting the Triforce. Defend Hyrule by fighting off the monsters send by Ganon that threaten the peace!</p>
+		  <img id='battleLinkPigHOW' src='https://pbs.twimg.com/media/Gdi3MJrXcAEwV_5?format=png&name=900x900'/>
+    </section>
 
-  <section>
-  <img id='battlefield' src='https://pbs.twimg.com/media/GaIpV4BXYAE9u-F?format=jpg&name=4096x4096'/>
-  </section>
+    <section id='howToSection2'>
+      <img id='battlefield' src='https://pbs.twimg.com/media/GaIpV4BXYAE9u-F?format=jpg&name=4096x4096'/>
+    </section>
   </section>
 
   `;
